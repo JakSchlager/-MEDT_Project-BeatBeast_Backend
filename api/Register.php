7876f375c24aa2a579bcea4d622305dd3d325475
JakSchlager <jami.schlager@gmail.com>
2023-06-07 16:02:42 +0200

    new telephone number validation instead of second password validation

--- api/Register.php
+++ api/Register.php
@@ -2,7 +2,7 @@
     $host = "localhost";
     $db   = "web";
     $user = "web";
-    $pass = "1234";
+    $pass = "1234"; 
     session_start();
     $conn = new mysqli($host, $user, $pass, $db);
 
@@ -23,17 +23,26 @@
     }
     else {
         header("location: http://localhost:5173/");
-        echo "Es wurde kein Passwort eingegeben!";
+        echo "Es wurde kein Emailadresse eingegeben!";
     }
 
-    if (isset($_POST['password1']) && !empty($_POST['password1'])){
-        $password = $_POST['password1'];
+    if (isset($_POST['password']) && !empty($_POST['password'])){
+        $password = $_POST['password'];
     }   
     else {
         Response::error(HttpErrorCodes::HTTP_BAD_REQUEST, "Missing parameters")->send();
     }
 
-    $statement = "INSERT INTO profiles (username, email, password) VALUES ('$username', '$email', '$password');";
+    if(isset($_POST['phoneNr']) && !empty($_POST['phoneNr'])) {
+        if(preg_match('/^[0-9]{10}+$/', $_POST['phoneNr'])) {
+            $phoneNr = $_POST['phoneNr'];
+        }   
+    }
+    else {
+        Response::error(HttpErrorCodes::HTTP_BAD_REQUEST, "Missing parameters")->send();
+    }
+
+    $statement = "INSERT INTO profiles (username, email, password, phoneNr) VALUES ('$username', '$email', '$password', '$phoneNr');";
     $conn->query($statement);
 
     header("location: http://localhost:5173/login?username=$username");
@@ -42,6 +51,6 @@
     if($res = self::$db->query($statement)) {
         while($row = $res->fetch_assoc()) {
             $myArray[] = $row;
-        }
+        }   
     }
 ?>
---
+++ api/UpdateProfile.php
@@ -0,0 +1,13 @@
+<?php
+    $host = "localhost";
+    $db   = "web";
+    $user = "web";
+    $pass = "1234"; 
+    session_start();
+    $conn = new mysqli($host, $user, $pass, $db);
+
+    if($conn->connect_error) {
+        die("connection failed ". $conn->connect_error);
+    }
+
+?>
